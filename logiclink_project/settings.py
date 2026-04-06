96a9c84ad27c7f3bd47419ea5bc40dfb20f08c45
achiko10 <achimgebr@yahoo.com>
2026-04-06 21:47:32 +0400

    security: move SECRET_KEY and DEBUG to environment variables

--- logiclink_project/settings.py
+++ logiclink_project/settings.py
@@ -1,3 +1,4 @@
+from decouple import config
 """
 Django settings for logiclink_project project.
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zwu#kyv*m#*ai7gxgfrtn_=194=c51l=il6ox8guk(u13pu0g="
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
